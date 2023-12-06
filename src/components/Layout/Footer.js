bc7f70dfc62d77d9dfe5b62525a8e33c8ffa99d0
Samiran-Pradhan <90782550+Samiran-Pradhan@users.noreply.github.com>
2023-12-06 23:27:29 +0530

    admit-wise

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+        <h1 className='text-center'>All Right Reserved</h1>
+        <p className='text-center mt-3'>
+        </p>
+    </div>
+  )
+}
+
+export default Footer
