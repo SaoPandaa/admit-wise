19d4fb9b9707ebe9b9ee8d8e2b12154af357b405
Samiran-Pradhan <90782550+Samiran-Pradhan@users.noreply.github.com>
2023-12-07 14:43:35 +0530

    admit-wise

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,41 +1,44 @@
-import React from 'react'
-import {NavLink,Link} from 'react-router-dom'
-import { GiShoppingCart } from "react-icons/gi";
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import {logo} from 'images'; 
 const Header = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <Link className="navbar-brand">
-      Admit Wise
-      </Link>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink to="/" className="nav-link">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/category" className="nav-link">Category</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/register" className="nav-link">Register</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/sign-up" className="nav-link">Sign-Up</NavLink>
-        </li>
-        {/* <li className="nav-item">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link className="navbar-brand">
+              <img
+                alt=""
+                src="../images/logo.png"
+              />
+            </Link>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/category" className="nav-link">Category</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/register" className="nav-link">Register</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/sign-up" className="nav-link">Sign-Up</NavLink>
+              </li>
+              {/* <li className="nav-item">
           <NavLink to="/cart" className="nav-link">Cart(0)</NavLink>
         </li> */}
-        
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-  
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+
     </>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,36 @@ import Layout from '../components/Layout/Layout'
 const HomePage = () => {
   return (
     <Layout>
-        <h1>home page</h1>
+      <div className="dummy-child" />
+      <div className="dummy-item" />
+      {/* <textarea
+        className="embark-on-your-container"
+        placeholder="Embark on Your Academic Journey with Confidence"
+      > */}
+      { /* <span className="embark-on-your">{`Embark on Your `}</span>
+        <span className="academic">Academic</span>
+        <span className="embark-on-your"> Journey with Confidence</span>
+      </textarea> */}
+      <div className="your-journey-starts">Your Journey Starts Here</div>
+      <textarea
+        className="our-seasoned-admission"
+        placeholder="Our seasoned admission consultants are dedicated to navigating the intricate pathways of college admissions. With personalized strategies and in-depth knowledge, we empower you to confidently pursue your dream education in renowned institutions."
+      />
+      <img className="dummy-inner" alt="" src="/group-5.svg" />
+      <img
+        className="beautiful-business-woman-with-icon"
+        alt=""
+        src="../images/hero.png"
+      />
+      <div className="rectangle-div" />
+      <div className="rectangle-group">
+        <div className="frame-child" />
+        <div className="frame-item" />
+        <div className="email-address-parent">
+          <div className="email-address">EMAIL ADDRESS</div>
+          <div className="book-an-appointment">Book an appointment</div>
+        </div>
+      </div>
     </Layout>
   )
 }
